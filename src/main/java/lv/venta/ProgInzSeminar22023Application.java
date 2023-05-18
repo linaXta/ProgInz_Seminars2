86ea894e2f84be6b1dd7b7b5acf9b9342e304cde
linaXta <alina.rubcova0418@gmail.com>
2023-05-18 09:29:06 +0200

    New Project for seminar2

---
+++ src/main/java/lv/venta/ProgInzSeminar22023Application.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgInzSeminar22023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgInzSeminar22023Application.class, args);
+	}
+
+}
