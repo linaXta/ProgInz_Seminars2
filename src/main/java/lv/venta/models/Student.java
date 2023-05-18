1bbc45f919e70d2b3e1dbfcb8f1aa0e2544dec48
linaXta <alina.rubcova0418@gmail.com>
2023-05-18 10:30:12 +0200

    New: Student class

--- src/main/java/lv/venta/models/Student.java
+++ src/main/java/lv/venta/models/Student.java
@@ -6,36 +6,28 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Table(name = "professor_table")
+@Table(name = "Student_table")
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString
-public class Professor {
+public class Student {
 	
-	
-	//TODO uzlikt Data JPA anotācijas (@Colum utt)
-	//TODO uzlikt atbilstošās validācijas
-
-	//TODO izveidot Cours klasi
-	//TODO izveidot Grade klasi
 	@Setter(value = AccessLevel.NONE)
-	@Column(name = "Idp") 
+	@Column(name = "Ids") 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long idp;
+	private long ids;
 	
 	@Column(name = "Name")
 	@NotNull
@@ -48,17 +40,12 @@ public class Professor {
 	@Pattern(regexp = "[A-ZĀĢČĒĪĶĻŅŠŪŽ]{1}[a-zāģčēīķļņšūž\\ ]+")
 	@Size(min = 3, max = 15)
 	private String surname;
-	
-	@Column(name = "Degree")
-	@NotBlank
-	private Degree degree;
 
-	public Professor(String name, String surname, Degree degree) {
-		super();
+	public Student(String name, String surname) {
 		this.name = name;
 		this.surname = surname;
-		this.degree = degree;
 	}
-
 	
+	
+
 }
