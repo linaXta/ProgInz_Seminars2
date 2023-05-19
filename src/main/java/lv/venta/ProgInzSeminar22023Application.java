313ca685c20fbf163081d35cf3302ea52891bd0b
linaXta <alina.rubcova0418@gmail.com>
2023-05-19 09:47:27 +0200

    Check many to many Entity for course and professors

--- src/main/java/lv/venta/ProgInzSeminar22023Application.java
+++ src/main/java/lv/venta/ProgInzSeminar22023Application.java
@@ -1,5 +1,8 @@
 package lv.venta;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -31,28 +34,40 @@ public class ProgInzSeminar22023Application {
 				
 				Professor pr1 = new Professor("Katrīna", "Šķirmante", Degree.mg);
 				Professor pr2 = new Professor("Mārtiņš", "Saulītis", Degree.mg);
+				Professor pr3 = new Professor("Jānis", "Baigasi", Degree.phd);
+				Professor pr4 = new Professor("Raita", "Rollande", Degree.mg);
 				profRepo.save(pr1);
 				profRepo.save(pr2);
+				profRepo.save(pr3);
+				profRepo.save(pr4);
 				
 				Student st1 = new Student("Jānis", "Bērziņš");
 				Student st2 = new Student("Ieva", "Kalniņa");
 				stRepo.save(st1);
 				stRepo.save(st2);
 				
-				//TODO Uztaisīt kursu, kam ir divi profesori
-				//TODO Uztaisīt gadījumu, kad viens profesors pasniedz divus kursus
-				Course c1 = new Course("Java", 4, pr1);
-				Course c2 = new Course("Datubāzes", 4, pr2);
+
+				Course c1 = new Course("Java", 4, new ArrayList<>(Arrays.asList(pr1, pr4)));
+				Course c2 = new Course("Datubāzes", 4, new ArrayList<>(Arrays.asList(pr2)));
+				Course c3 = new Course("Programmatūras inženierija", 4, new ArrayList<>(Arrays.asList(pr3, pr4)));
 				crRepo.save(c1);
 				crRepo.save(c2);
+				crRepo.save(c3);
+				
+				c1.addProfessor(pr1);
+				c1.addProfessor(pr4);
+				c2.addProfessor(pr2);
+				c3.addProfessor(pr3);
+				c3.addProfessor(pr4);
+				crRepo.save(c1);
+				crRepo.save(c2);
+				crRepo.save(c3);
 				
 				grRepo.save(new Grade(10, st1, c1));
 				grRepo.save(new Grade(7, st1, c2));
 				grRepo.save(new Grade(6, st2, c1));
 				grRepo.save(new Grade(5, st2, c2));
 				
-				//TODO pārveidot saiti starp professor un kursu uz ManyToMany
-				//TODO pamainīt testMode funkciju 1.professoram 2 kursus un 1.kurasm, ka to pasniedz abi profesori
 				
 			}
 		};
